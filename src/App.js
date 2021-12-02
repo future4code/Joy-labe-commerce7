1651c54c5849522d2c719665331e551ed08e2373
dnlMatos <georginedanilo@gmail..com>
2021-12-02 00:05:35 -0300

    filter feito

--- src/App.js
+++ src/App.js
@@ -7,63 +7,119 @@ class App extends React.Component {
     cards: [
       {
         id: Date.now(),
-        tituloProduto: "testes",
+        tituloProduto: "arroz",
         valor: 10000,
         img: "https://picsum.photos/400/400?a=1",
       },
       {
         id: Date.now(),
-        tituloProduto: "testes2",
+        tituloProduto: "macarrao",
         valor: 20,
         img: "https://picsum.photos/400/400?a=2",
       },
       {
         id: Date.now(),
-        tituloProduto: "testes3",
+        tituloProduto: "brócolis",
+        valor: 210,
+        img: "https://picsum.photos/400/400?a=4",
+      },
+      {
+        id: Date.now(),
+        tituloProduto: "pera",
         valor: 300,
         img: "https://picsum.photos/400/400?a=3",
       },
     ],
-    filtro: "",
+    selectOrdenado: "Nenhum",
+    inputNome: "",
+    inputMinimo: "",
+    inputMaximo: "",
   };
 
-  mudaFiltro = (e) => {
-    this.setState({ filtro: e.target.value });
+  atualizaInputNome = (e) => {
+    this.setState({ inputNome: e.target.value });
+  };
+
+  atualizaInputMinimo = (e) => {
+    this.setState({ inputMinimo: e.target.value });
+  };
+
+  atualizaInputMaximo = (e) => {
+    this.setState({ inputMaximo: e.target.value });
+  };
+
+  selectOrdenado = (e) => {
+    this.setState({ selectOrdenado: e.target.value });
   };
 
   render() {
-    const listaCards = this.state.cards.map((card, index) => {
-      console.log(card);
-      return (
-        <Card
-          key={index}
-          imagem={card.img}
-          nomeProduto={card.tituloProduto}
-          precoProduto={"R$" + card.valor}
-        />
-      );
-    });
-
-    const cardsFiltrados = this.state.cards.sort((a, b) => {
-      switch (this.state.filtro) {
-        case "Crescente":
-          console.log('crescente')
-          return a.valor > b.valor;
+    const listaCards = this.state.cards
+      .filter((produto) => {
+        //filtra o array seguindo escolha do usuario
+        return produto.tituloProduto
+          .toLocaleLowerCase()
+          .includes(this.state.inputNome.toLocaleLowerCase());
+      })
+      .filter(produto => {//fitro usando valor min
+        return this.state.inputMinimo ==='' || produto.valor >= this.state.inputMinimo
+      })
+      .filter(produto => {//fitro usando valor max
+        return this.state.inputMaximo ==='' || produto.valor <= this.state.inputMaximo
+      })
+      .sort((produtoDaVez,produtoSeguinte) => {//filtro do select
+        switch (this.state.selectOrdenado) {
+          case "Crescente":
+            return produtoDaVez.valor - produtoSeguinte.valor
           case "Decrescente":
-            console.log('decrescente')
-          return a.valor < b.valor;
-        default:
-          return 0;
-      }
-    });
+            return produtoDaVez.valor + produtoSeguinte.valor
+          case "Titulo":
+            return produtoDaVez.tituloProduto.localeCompare(produtoSeguinte.tituloProduto) //ordenação por titulo
+          default:
+            return 0;
+        }
+      })
+      .map((card, index) => {
+        //após filtra a busca, há renderização dos elementos do array
+        return (
+          <Card
+            key={index}
+            imagem={card.img}
+            nomeProduto={card.tituloProduto}
+            precoProduto={"R$" + card.valor}
+          />
+        );
+      });
+
     return (
       <div className="App">
-        <label>Filtro:</label>
-        <select value={this.state.filtro} onChange={this.mudaFiltro}>
-          <option value="">Nenhum</option>
-          <option value="Crescente">Crescente</option>
-          <option value="Decrescente">Decrescente</option>
-        </select>
+        <div>
+          <label for='ordemSelect'>Ordenar por:</label>
+          <select name='ordemSelect' value={this.state.selectOrdenado} onChange={this.selectOrdenado} value={this.state.selectOrdenado}>
+            <option value="">Nenhum</option>
+            <option value="Crescente">Crescente</option>
+            <option value="Decrescente">Decrescente</option>
+            <option value="Titulo">Título</option>
+          </select>
+        </div>
+        <div>
+          <input
+            placeholder="Busque pelo produto"
+            value={this.state.inputNome}
+            onChange={this.atualizaInputNome}
+          />
+          <input
+            type='number'
+            placeholder="Valor mínimo do produto"
+            value={this.state.inputMinimo}
+            onChange={this.atualizaInputMinimo}
+            />
+          <input
+            type='number'
+            placeholder="Valor máximo do produto"
+            value={this.state.inputMaximo}
+            onChange={this.atualizaInputMaximo}
+          />
+        </div>
         <div className="card">{listaCards}</div>
         <div className="carrinho">
           <Carrinho />
